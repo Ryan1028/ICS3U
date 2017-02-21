7dd0109aff1351844b3a64fc988de384ee2b0cda
Ryan Seftel <324902634@C-9S608J1.tdsb.on.ca>
2017-02-21 14:45:03 -0500

    Pushing My First program

---
+++ Hello/src/seftel/HelloWorld.java
@@ -0,0 +1,17 @@
+package seftel;
+/**
+ * Hello World
+ * Prints Hello World
+ * 21/02/2017
+ * @author Ryan Seftel
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		
+
+	}
+
+}
